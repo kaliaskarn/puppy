08b1cf482b130a9b3f675710877b484f64e69e01
Kaliaskar Nurlan <maraqfam@gmail.com>
2023-12-23 20:19:52 +0200

    first time testing git tags

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/kaliaskarn/dog"
 )
 
@@ -18,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func From12() {
+	fmt.Println("this is for version 1.0.0")
 }
